7de01b56aeffb09fa5fdd65a09eca2e715e854e5
denis.marques <denis.marques@icolabora.com.br>
2018-04-16 21:54:33 -0300

    14/04/2018

---
+++ src/main/java/AssessmentTest18/UsingLocalDate.java
@@ -0,0 +1,33 @@
+package AssessmentTest18;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+
+/**
+ * Created by denis on 14/04/2018.
+ */
+public class UsingLocalDate {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.of(2015, Calendar.APRIL, 1)); // 2015-03-01
+        System.out.println(LocalDate.of(2015, Month.APRIL, 1));    // 2015-04-01
+        System.out.println(LocalDate.of(2015, 3, 1));              // 2015-03-01
+        System.out.println(LocalDate.of(2015, 4, 1));              // 2015-04-01
+        // private method System.out.println(new LocalDate(2015, 3, 1));
+        // private method System.out.println(new LocalDate(2015, 4, 1));
+    }
+}
+
+/*
+
+A. System.out.println(LocalDate.of(2015, Calendar.APRIL, 1));
+B. System.out.println(LocalDate.of(2015, Month.APRIL, 1));
+C. System.out.println(LocalDate.of(2015, 3, 1));
+D. System.out.println(LocalDate.of(2015, 4, 1));
+E. System.out.println(new LocalDate(2015, 3, 1));
+F. System.out.println(new LocalDate(2015, 4, 1));
+
+R:   B, D
+
+*/
